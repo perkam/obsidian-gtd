78ae65bd34d14f4f00f1476838b0e64ed517406a
kacper <corgodev@gmail.com>
2026-03-26 13:34:45 +0100

    feat: fix people mention table

--- Scripts/Dataview/mentions-table.js
+++ Scripts/Dataview/mentions-table.js
@@ -1,19 +1,30 @@
 if (input) {
-	// Add a header of level X, e.g. input = 2 means header level 2
-	const name = dv.current().file.name.replace(/^@/, '')
-	dv.header(input, 'Tasks referencing ' + name)
+  // Add a header of level X, e.g. input = 2 means header level 2
+  const name = dv.current().file.name.replace(/^@/, "");
+  dv.header(input, "Tasks referencing " + name);
 }
 
 function tasksMentioningPerson(tasks, person) {
-	console.log(tasks[0])
-	return tasks.filter((task) => task.text.includes(person)).map((task) => task.text)
+  return tasks
+    .filter((task) => task.text.includes(person))
+    .map((task) => task.text);
 }
 
-const person = dv.current().file.name
-const pages = dv.pages("[[" + person + "]]").filter((page) => page.file.path !== "01 Project Management/🗄️ Completed tasks.md")
-const tasks = pages.map(b => [b.file.link, tasksMentioningPerson(b.file.tasks, person)]).flatMap(([key, values]) => values.map(value => [key, value]));
+const person = dv.current().file.name;
 
-dv.table(
-	["Note", "Task"],
-	tasks
-)
+// Updated filter logic below
+const pages = dv.pages("[[" + person + "]]").filter((page) => {
+  const isCompletedFolder = page.file.path.startsWith(
+    "01 Project Management/Projects/✔ Completed",
+  );
+  const isArchiveFile =
+    page.file.path === "01 Project Management/🗄️ Completed tasks.md";
+
+  return !isCompletedFolder && !isArchiveFile;
+});
+
+const tasks = pages
+  .map((b) => [b.file.link, tasksMentioningPerson(b.file.tasks, person)])
+  .flatMap(([key, values]) => values.map((value) => [key, value]));
+
+dv.table(["Note", "Task"], tasks);
